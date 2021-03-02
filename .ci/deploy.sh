77673e4079293babcf9aed34334731d0819cd2f1
Zhixiang Wu <Zhixiang.Wu@nike.com>
2021-03-02 20:46:00 +0800

    update

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -19,19 +19,26 @@ while getopts "r:b:s:c:" opt; do
   esac
 done
 
-s3objects=`aws s3api list-objects --bucket $s3bucket --output json --query "length(Contents[])" $cliparams`
-if [ "$s3objects" != "null" -a $s3objects > 0 ];then
+aws s3api list-objects --bucket $s3bucket --output json --query "length(Contents[])" $cliparams || not_empty=true
+# sync github to s3
+if [ $not_empty ];then
   echo "Bucket not empty. Start incremental sync."
-  git diff-tree --no-commit-id --name-status -r $sha | while read status filename; do
+  sha_before=`aws s3 cp s3://$s3bucket/SYNCED_SHA - $cliparams | cat || ""`
+  git diff-tree --no-commit-id --name-status -r $sha $sha_before | while read status filename; do
   if [ $status = "D" ]; then
     #echo "Delete $filename from S3"
-    aws s3 rm "s3://$s3bucket/$filename" --exclude ".git/*" --exclude ".ci/*" $cliparams
+    aws s3 rm "s3://$s3bucket/$filename" $cliparams
   else
     #echo "Copy $filename to S3"
-    aws s3 cp "$filename" "s3://$s3bucket/$filename" --exclude ".git/*" --exclude ".ci/*" --recursive $cliparams
+    aws s3 cp "$filename" "s3://$s3bucket/$filename" --exclude ".git/*" --exclude ".ci/*" --recursive $cliparams || echo "Skip $filename."
   fi
   done
 else
   echo "Bucket is empty. Start first sync."
   aws s3 sync . "s3://$s3bucket" --exclude ".git/*" --exclude ".ci/*" $cliparams
 fi
+
+# save synced sha
+echo $sha > SYNCED_SHA
+aws s3 cp SYNCED_SHA "s3://$s3bucket/" $cliparams
+
